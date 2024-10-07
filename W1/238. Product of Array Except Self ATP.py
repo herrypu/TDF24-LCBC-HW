68e3c9ea67a216ffdc1ff1bd363114ec9bf526b2
herrypu <wbpht2@163.com>
2024-10-06 23:10:02 -0400

    another try on 238 failed

--- W1/238. Product of Array Except Self ATP.py
+++ W1/238. Product of Array Except Self ATP.py
@@ -1,6 +1,17 @@
 #238. Product of Array Except Self
 #https://leetcode.com/problems/product-of-array-except-self/description/
+#参考solution ‘Simple and easy 1 min explanation in Python and JAVA’
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        p=[]
+        lp=1
+        rp=1
+        i=0
+        pes=[1]*len(nums)
+        while i < len(nums):
+            lp *= nums[i]
+            rp *= nums[-i]
+            pes[i] *= lp
+            pes[-i] *= rp
+            i += 1
+        return pes
